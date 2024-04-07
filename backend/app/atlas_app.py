98bc1ed32cb57795e50d9a60ed0101c887aef3f5
Daniel Medina <daniel.m@selia.co>
2024-04-06 22:03:49 -0400

    fixes

--- backend/app/atlas_app.py
+++ backend/app/atlas_app.py
@@ -1,8 +1,9 @@
-from backend.app.atlas_app import create_app
+# atlas_app.py
+from app import create_app  # Adjust the import according to your actual package structure
 import os
 
 backend = create_app()
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    backend.run(host='0.0.0.0', port=port, debug=False)  # Set debug=False for production
+    backend.run(host='0.0.0.0', port=port, debug=False)
