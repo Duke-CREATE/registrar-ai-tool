7a1a21c1ff203af6cadf06c46c2f1d4f81d7a38c
Daniel Medina <daniel.m@selia.co>
2024-04-07 11:14:06 -0400

    modified cors

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -2,29 +2,28 @@
 from flask import Flask
 from flask_cors import CORS
 from flask_caching import Cache
-from .config import Config  # Assuming you have a separate production configuration
+from .config import Config
 
-def create_app(config_class=Config):  # Change to use ProductionConfig for production
+def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
     
-    # Initialize Flask-CORS
-    cors = CORS(app)
+    # Setup CORS for all routes but specify the origins for security
+    # You can also restrict this to specific routes
+    CORS(app, resources={r"/api/*": {"origins": "https://atlas-frontend-two.vercel.app"}})
+
     app.config['CORS_HEADERS'] = 'Content-Type'
 
-    # Configure and initialize caching to use Redis
-    # Ensure these settings are correct for your Redis setup
-    app.config['CACHE_TYPE'] = 'RedisCache'  # Change from 'simple' to 'RedisCache'
-    app.config['CACHE_REDIS_URL'] = app.config.get('REDIS_URL')  # Use REDIS_URL from your config, or fallback to default
+    # Configure caching and other app features
+    app.config['CACHE_TYPE'] = 'RedisCache'
+    app.config['CACHE_REDIS_URL'] = app.config.get('REDIS_URL')
 
     cache = Cache(app)
 
-    # Import and register your blueprints or routes
+    # Register blueprints
     from .routes import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
-    # Make cache accessible through app for route-specific caching
     app.cache = cache
 
     return app
-
